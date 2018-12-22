a397193704a60ae038646fd940df391b9a46ecc0
Jens Gabrikowski <jens@gabrikowski.de>
2018-12-22 17:45:25 +0100

    Enable audio UI feedback.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1515,8 +1515,8 @@
 // Note: Test audio output with the G-Code:
 //  M300 S<frequency Hz> P<duration ms>
 //
-//#define LCD_FEEDBACK_FREQUENCY_DURATION_MS 2
-//#define LCD_FEEDBACK_FREQUENCY_HZ 5000
+#define LCD_FEEDBACK_FREQUENCY_DURATION_MS 4
+#define LCD_FEEDBACK_FREQUENCY_HZ 1000
 
 //=============================================================================
 //======================== LCD / Controller Selection =========================
